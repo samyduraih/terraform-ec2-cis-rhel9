1ad68c23241324dee77f4cb1c462f171ff20d836
samyduraih <110995724+samyduraih@users.noreply.github.com>
2025-05-07 23:31:41 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,9 +5,7 @@ variable "security_group_ids" {
   type = list(string)
 }
 variable "key_name" {}
-variable "associate_public_ip" {
-  default = false
-}
+variable "associate_public_ip" {}
 variable "instance_name" {}
 variable "tags" {
   type = map(string)
