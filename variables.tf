f56c26a13c6e54c52c7a909d816b0a5dfc3f40ee
samyduraih <110995724+samyduraih@users.noreply.github.com>
2025-05-07 23:33:18 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,39 @@
-variable "aws_region" {}
-variable "instance_type" {}
-variable "subnet_id" {}
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.medium"
+}
+
+variable "subnet_id" {
+  type = string
+}
+
 variable "security_group_ids" {
   type = list(string)
 }
-variable "key_name" {}
-variable "associate_public_ip" {}
-variable "instance_name" {}
+
+variable "key_name" {
+  type = string
+}
+
+variable "associate_public_ip" {
+  type    = bool
+  default = false
+}
+
+variable "instance_name" {
+  type = string
+}
+
 variable "tags" {
-  type = map(string)
+  type    = map(string)
+  default = {}
+}
+
+variable "user_data" {
+  type    = string
+  default = ""
 }
